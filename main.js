1419342b193ba1a6300e447ccbba9275dfc061c8
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-10 16:41:08 -0700

    menu & global shortcuts for pc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 
 // Set enviroment
@@ -10,6 +10,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -26,31 +27,71 @@ function createMainWindow() {
       mainWindow.loadFile('./app/index.html')
     }
 
+    function createAboutWindow() {
+      aboutWindow = new BrowserWindow({
+        title: 'About ImageShrinker',
+        width: 300,
+        height: 300,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: false,
+        backgroundColor: 'white',
+      })
+    
+      aboutWindow.loadFile('./app/about.html')
+    }
+
+
     app.on('ready', () => {
       createMainWindow()
     
        const mainMenu = Menu.buildFromTemplate(menu)
-       Menu.setApplicationMenu(mainMenu)
-
-       globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-       globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Chift+I', () => mainWindow.toggleDevTools())
-    
+       Menu.setApplicationMenu(mainMenu) 
       mainWindow.on('closed', () => (mainWindow = null))
     })
 
 const menu = [
-  ...(isMac ? [{ role: 'appmenu' }] : []),
-  {
-    label: 'File',
+  ...(isMac ? [{ 
+    label: app.name,
     submenu: [
       {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+W',
-        click: () => app.quit(),
+        label: 'About',
+        click: createAboutWindow,
       },
     ],
+   }] : []),
 
+   {
+    role: 'fileMenu',
   },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+    
+  {
+    role: 'fileMenu',
+  },
+  ...(isDev ? [
+      {
+        label: 'Developer',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { type: 'separator' },
+          { role: 'toggledevtools' },
+        ],
+      },
+    ]
+  : []),
 ]
 
     app.on('window-all-closed', () => {
