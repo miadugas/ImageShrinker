493f37dc1e6047784e80745022614f36ebcbe1fc
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-10 15:02:44 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+      title: 'ImageShrinker',
+      width: 500,
+      height: 600
+    //   icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    //   resizable: isDev ? true : false,
+    //   backgroundColor: 'white',
+    //   webPreferences: {
+    //     nodeIntegration: true,
+      })
+    }
+
+    app.on('ready', createMainWindow)
+
