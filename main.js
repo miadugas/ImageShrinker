77ef492902201c33837002d19af07076c785a2c6
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-10 15:16:56 -0700

    icons added

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ function createMainWindow() {
     mainWindow = new BrowserWindow({
       title: 'ImageShrinker',
       width: 500,
-      height: 600
-    //   icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+      height: 600,
+      icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     //   resizable: isDev ? true : false,
     //   backgroundColor: 'white',
     //   webPreferences: {
     //     nodeIntegration: true,
       })
+
+      mainWindow.loadFile('./app/index.html')
     }
 
     app.on('ready', createMainWindow)
